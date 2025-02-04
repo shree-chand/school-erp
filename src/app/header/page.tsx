ef5d5b87fbdcf0ed12b7622ced62dcd95c7041e4
Shree Chand <shree.chand@hostbooks.com>
2025-02-04 15:32:20 +0530

    formating

--- src/app/header/page.tsx
+++ src/app/header/page.tsx
@@ -7,7 +7,7 @@ export default function Header({ toggleSidebar }: { toggleSidebar: () => void })
             <div className="mainHeader">
                 <header id="header" className="header fixed-top d-flex align-items-center">
                     <div className="d-flex align-items-center justify-content-between">
-                    <i className="bi bi-list toggle-sidebar-btn" onClick={toggleSidebar}></i>
+                        <i className="bi bi-list toggle-sidebar-btn" onClick={toggleSidebar}></i>
                         <a href="/dashboard" className="logo d-flex align-items-center">
                             <img src="/image/logo.png" alt="" />
                         </a>
@@ -169,62 +169,62 @@ export default function Header({ toggleSidebar }: { toggleSidebar: () => void })
                             </li>
 
                             <li className="nav-item dropdown pe-3">
-  <a
-    className="nav-link nav-profile d-flex align-items-center pe-0"
-    href="#"
-    data-bs-toggle="dropdown"
-  >
-    <img src="/image/profile-img.jpg" alt="Profile" className="rounded-circle" />
-    <span className="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
-  </a>
+                                <a
+                                    className="nav-link nav-profile d-flex align-items-center pe-0"
+                                    href="#"
+                                    data-bs-toggle="dropdown"
+                                >
+                                    <img src="/image/profile-img.jpg" alt="Profile" className="rounded-circle" />
+                                    <span className="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
+                                </a>
 
-  <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
-    <li className="dropdown-header">
-      <h6>Kevin Anderson</h6>
-      <span>Web Designer</span>
-    </li>
-    <li>
-      <hr className="dropdown-divider" />
-    </li>
+                                <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
+                                    <li className="dropdown-header">
+                                        <h6>Kevin Anderson</h6>
+                                        <span>Web Designer</span>
+                                    </li>
+                                    <li>
+                                        <hr className="dropdown-divider" />
+                                    </li>
 
-    <li>
-      <a className="dropdown-item d-flex align-items-center" href="users-profile.html">
-        <i className="bi bi-person"></i>
-        <span>My Profile</span>
-      </a>
-    </li>
-    <li>
-      <hr className="dropdown-divider" />
-    </li>
+                                    <li>
+                                        <a className="dropdown-item d-flex align-items-center" href="users-profile.html">
+                                            <i className="bi bi-person"></i>
+                                            <span>My Profile</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <hr className="dropdown-divider" />
+                                    </li>
 
-    <li>
-      <a className="dropdown-item d-flex align-items-center" href="users-profile.html">
-        <i className="bi bi-gear"></i>
-        <span>Account Settings</span>
-      </a>
-    </li>
-    <li>
-      <hr className="dropdown-divider" />
-    </li>
+                                    <li>
+                                        <a className="dropdown-item d-flex align-items-center" href="users-profile.html">
+                                            <i className="bi bi-gear"></i>
+                                            <span>Account Settings</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <hr className="dropdown-divider" />
+                                    </li>
 
-    <li>
-      <a className="dropdown-item d-flex align-items-center" href="pages-faq.html">
-        <i className="bi bi-question-circle"></i>
-        <span>Need Help?</span>
-      </a>
-    </li>
-    <li>
-      <hr className="dropdown-divider" />
-    </li>
+                                    <li>
+                                        <a className="dropdown-item d-flex align-items-center" href="pages-faq.html">
+                                            <i className="bi bi-question-circle"></i>
+                                            <span>Need Help?</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <hr className="dropdown-divider" />
+                                    </li>
 
-    <li>
-      <a className="dropdown-item d-flex align-items-center" href="#">
-        <i className="bi bi-box-arrow-right"></i>
-        <span>Sign Out</span>
-      </a>
-    </li>
-  </ul>
-</li>
+                                    <li>
+                                        <a className="dropdown-item d-flex align-items-center" href="#">
+                                            <i className="bi bi-box-arrow-right"></i>
+                                            <span>Sign Out</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
 
 
                         </ul>
